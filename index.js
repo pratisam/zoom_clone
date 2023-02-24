b641c420e982df79f683a7d0e1a76f364621f385
prati <pratima.sampath@gmail.com>
2023-02-24 16:28:41 +0100

    setup

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ let localStream
 let remoteStream
 let peerConnection
 
+const server = {
+    iceServers:[{
+        urls:['stun:stun1.l.google.com:19302','stun:stun2.l.google.com:19302']
+    }]
+}
+
 let init =async() => {
     localStream = await navigator.mediaDevices.getUserMedia({
         video:true,
@@ -14,9 +20,27 @@ let init =async() => {
 }
 let createOffer = async() =>{
     //A WebRTC connection between the local computer and a remote peer. 
-    peerConnection =new RTCPeerConnection()
+    //passing the server to the rtcpeerconnection
+    peerConnection =new RTCPeerConnection(server)
     remoteStream = new MediaStream()
     document.getElementById("user2").srcObject = remoteStream
+
+    //tracking all the stream
+    localStream.getTracks().forEach((track) => {
+        peerConnection.addTrack(track,localStream)
+    });
+    //tracking remote stream
+    peerConnection.ontrack =(e) =>{
+        e.streams[0].getTracks().forEach((track) => {
+            remoteStream.addTrack()
+        })
+    }
+
+    peerConnection.onicecandidate = async (e) => {
+        if(e.candidate){
+            console.log("new ice candidate",e.candidate)
+        }
+    }
     let offer = await peerConnection.createOffer()
     await peerConnection.setLocalDescription(offer)
     console.log("offer:",offer)
